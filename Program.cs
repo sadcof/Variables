47e2baa267ae4b669f7619e47475fb25e56fb267
sadcof <sadsidecoff@gmail.com>
2023-04-10 17:22:04 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 10f;
+            float b = 38f;
+            float c = (31 - 5 * a)/b;
+            Console.WriteLine(c);
+            Console.ReadKey();  
+        }
+    }
+}
